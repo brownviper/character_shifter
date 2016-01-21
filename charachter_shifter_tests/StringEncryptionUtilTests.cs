b70a4dc41dd124758f8795cf12fcb855b6f8e4c2
Saz <s4saz@hotmail.com>
2016-01-21 19:06:05 +0000

    now lets take care of duplication

--- charachter_shifter_tests/StringEncryptionUtilTests.cs
+++ charachter_shifter_tests/StringEncryptionUtilTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
 using StringEncryptionUtil;
 
 namespace charachter_shifter_tests
@@ -6,17 +7,23 @@ namespace charachter_shifter_tests
     [TestFixture]
     public class StringEncryptionUtilTests
     {
+        private ShiftingUtil shiftingUtil;
+
+        [SetUp]
+        public void setup()
+        {
+            shiftingUtil = new ShiftingUtil();
+        }
+
         [Test]
         public void encrypt_return_input_string()
         {
-            var shiftingUtil = new ShiftingUtil();
             Assert.AreEqual(shiftingUtil.encrypt("a", 0), "a");
         }
 
         [Test]
         public void encrypt_will_return_shifted_input_string_by_shift_key()
         {
-            var shiftingUtil = new ShiftingUtil();
             Assert.AreEqual(shiftingUtil.encrypt("a", 1), "b");
         }
     
