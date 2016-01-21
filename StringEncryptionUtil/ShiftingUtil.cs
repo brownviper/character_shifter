d57028d593b935f432f5b5c1ad3adfe0a4a52154
Saz <s4saz@hotmail.com>
2016-01-21 16:56:46 +0000

    now test for creating a blank util class is added

---
+++ StringEncryptionUtil/ShiftingUtil.cs
@@ -0,0 +1,6 @@
+namespace StringEncryptionUtil
+{
+    public class ShiftingUtil
+    {
+    }
+}
---
+++ charachter_shifter_tests/StringEncryptionUtilTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using StringEncryptionUtil;
+
+namespace charachter_shifter_tests
+{
+    [TestFixture]
+    public class StringEncryptionUtilTests
+    {
+        [Test]
+        public void _Test()
+        {
+            var shiftingUtil = new ShiftingUtil();
+            Assert.NotNull(shiftingUtil);
+        }
+    }
+}
